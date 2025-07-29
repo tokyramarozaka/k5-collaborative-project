053c75c59f8b68ff74008f962a8caa05f0689bcb
tokyramarozaka <tokyramarozaka@gmail.com>
2025-07-29 11:27:48 +0300

    feat: add robot tests

---
+++ src/test/java/robot/RobotTest.java
@@ -0,0 +1,48 @@
+package robot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotTest {
+    @Test
+    void initializeRobot_without_argument_is_at_0_0_andPointsNorth_ok() {
+        var robot = new Robot();
+
+        assertEquals(0, robot.getX());
+        assertEquals(0, robot.getY());
+        assertEquals(Direction.NORTH, robot.getDirection());
+    }
+
+    @Test
+    void initializeRobot_with_arguments_ok() {
+        var robot = new Robot(15, 20, Direction.WEST);
+
+        assertEquals(15, robot.getX());
+        assertEquals(20, robot.getY());
+        assertEquals(Direction.WEST, robot.getDirection());
+    }
+
+    @Test
+    void turnRight_ok() {
+        var robot = new Robot(10, 10, Direction.EAST);
+
+        robot.turnRight();
+        assertEquals(Direction.SOUTH, robot.getDirection());
+
+        robot.turnRight();
+        assertEquals(Direction.WEST, robot.getDirection());
+
+        robot.turnRight();
+        assertEquals(Direction.NORTH, robot.getDirection());
+    }
+
+    @Test
+    void advance_ok() {
+        var robot = new Robot(0,0, Direction.EAST);
+
+        robot.advance();
+        assertEquals(1, robot.getX());
+        assertEquals(0, robot.getY());
+    }
+}
