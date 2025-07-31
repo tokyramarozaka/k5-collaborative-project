0cbb5c7244fa621761d5bb3b9ab1502dd19410dd
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2025-07-31 13:45:21 +0300

    feat: test

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -1,4 +1,50 @@
 package robot;
 
 public class Robot {
+    private  int x;
+    private  int y;
+    private Directions direction;
+
+    public Robot(int x, int y, Directions direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    //-------------getters-------------
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Directions getDirection() {
+        return direction;
+    }
+
+    //---------------------COdes-----------------------
+
+    static void advance(){
+
+    }
+
+    static void turnLeft(){
+
+    }
+
+    static void turnRight(){
+
+    }
+
+    static void stepBack(){
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
 }
